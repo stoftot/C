9e6cf661aecfbf0f0a4b9c26782e1c07edfe9a17
stoftot <gryn@itu.dk>
2025-06-03 18:36:57 +0200

    Created simpel calculator

---
+++ Simple calculator/main.c
@@ -0,0 +1,34 @@
+﻿#include <stdbool.h>
+#include <stdio.h>
+
+int main(void) {
+    while (true) {
+        double a, b;
+        char op;
+        printf("Enter two numbers: ");
+        scanf("%lf %lf", &a, &b);
+        printf("\nEnter operator: ");
+        scanf(" %c", &op);
+
+        switch (op) {
+            case '+':
+                printf("%.2lf + %.2lf = %.2lf", a, b, a+b);
+                break;
+            case '-':
+                printf("%.2lf - %.2lf = %.2lf", a, b, a-b);
+                break;
+            case '*':
+                printf("%.2lf * %.2lf = %.2lf", a, b, a*b);
+                break;
+            case '/':
+                printf("%.2lf / %.2lf = %.2lf", a, b, a/b);
+                break;
+            default:
+                printf("Non valid operator");
+                break;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
